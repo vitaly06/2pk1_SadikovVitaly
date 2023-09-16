6a7543eeb94a2564add1cea0390b95a56353cdb4
Виталий Садиков <vitaly.sadikov1@yandex.ru>
2023-09-16 08:34:04 +0300

    Добавлена 3 практика

---
+++ PZ_03/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PZ_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Отгадайте загадку:");
+            Console.WriteLine("Вокруг носа вьется, а в руки не дается.");
+            string answer = "";
+            while (!answer.Equals("запах") && !answer.Equals("сдаюсь")) 
+            {
+                Console.Write("Введите ответ: ");
+                answer = Console.ReadLine().ToLower();
+                switch (answer)
+                {
+                    case "запах":
+                        Console.WriteLine("Это верный ответ");
+                        break;
+                    case "сдаюсь":
+                        Console.WriteLine("Правильный ответ: запах.");
+                        break;
+                    default:
+                        Console.WriteLine("Неверно. Попробуйте ещё раз");
+                        break;
+                }
+            }            
+        }
+    }
+}
