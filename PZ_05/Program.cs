c462743f734b9ea938f6f6f6a3af9a5078bd7012
Виталий Садиков <vitaly.sadikov1@yandex.ru>
2023-09-22 22:26:00 +0500

    Добавлена пятая практика

---
+++ PZ_05/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 1
+            Console.WriteLine("Таблица умножения на 7:");
+            int z = 0;
+            while(z <= 10)
+            {
+                Console.WriteLine($"7 * {z} = {z * 7}");
+                z++;
+            }
+            Console.WriteLine("***************");
+            // Задание 2
+            Console.Write("Введите целое положительное число n: ");
+            int n  = Convert.ToInt32(Console.ReadLine());
+            if (n < 0)
+            {
+                Console.WriteLine("Вы ввели отрицательное число!");
+            }
+            else
+            {
+                int sum = 0;
+                int i = 0;
+                while (i <= n)
+                {
+                    sum += i;
+                    i++;
+                }
+                Console.WriteLine($"Сумма чисел от 0 до {n}: {sum}");
+            }           
+        }
+    }
+}
