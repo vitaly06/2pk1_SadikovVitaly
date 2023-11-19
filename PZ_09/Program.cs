b7ec25e762dfd893bd31fd3f48cd089bd69f267c
Виталий Садиков <vitaly.sadikov1@yandex.ru>
2023-11-19 11:50:38 +0500

    Добавлена 9 практика.

---
+++ PZ_09/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace PZ_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вариант 20
+            Console.Write("Введите строку: ");
+            string str = Console.ReadLine();
+            string[] words = str.Split(new[] { ' ', ',', '.' }, StringSplitOptions.RemoveEmptyEntries);
+            Array.Sort(words);
+            var uniqueWords = words.DistinctBy(w => w.Length);
+            string result = string.Join(" ", uniqueWords);
+            Console.WriteLine(result);
+        }
+    }
+}
