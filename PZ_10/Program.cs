fa1b11762747a2811dcb736798d3462ccf2d1766
Виталий Садиков <vitaly.sadikov1@yandex.ru>
2023-11-24 17:55:25 +0500

    Добавлена 10 практика

---
+++ PZ_10/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PZ_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вариант 20
+            string raw = Console.ReadLine();
+            var items = raw.Split(' ');
+            double sum = 0;
+            foreach (var i in items)
+            {
+                int test = 0;
+                double test1 = 0.0;
+                if (int.TryParse(i, out test))
+                {
+                    sum += test;
+                }
+                else if(double.TryParse(i, out test1))
+                {
+                    sum += test1;
+                }
+            }
+            Console.WriteLine($"Сумма для оплаты: {sum}");
+        }
+    }
+}
