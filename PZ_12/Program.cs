73921f23b5532fae275bf9ec8d487f97bbbea9a2
Виталий Садиков <vitaly.sadikov1@yandex.ru>
2023-12-01 17:28:12 +0500

    Добавлена 12 практика

---
+++ PZ_12/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PZ_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вариант 20
+            double[] temperatures = new double[7];
+            // Вводим данные в массив с клавиатуры
+            for (int i = 0; i < 7; i++)
+            {
+                Console.Write($"Введите температуру в {i + 1} день: ");
+                temperatures[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            Console.WriteLine($"Количество дней с минусовой температурой: {minusDays(temperatures)}");
+        }
+
+        static int minusDays(double[] temps)
+        {
+            int count = 0;
+            foreach(double temp in temps)
+            {
+                if (temp < 0)
+                {
+                    count += 1;
+                }
+            }
+            return count;
+        }
+    }
+}
