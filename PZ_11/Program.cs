028133640407b17ce102c807794d7fd580ea507f
Виталий Садиков <vitaly.sadikov1@yandex.ru>
2023-12-01 17:01:58 +0500

    Добавлена 11 практика

---
+++ PZ_11/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace PZ_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вариант 20
+            double AMean, GMean;
+            double A, B, C, D;
+            Console.Write("Введите A: ");
+            A = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите B: ");
+            B = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите C: ");
+            C = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите D: ");
+            D = Convert.ToDouble(Console.ReadLine());
+            Mean(A, B, out AMean, out GMean);
+            Console.WriteLine("A, B:");
+            Console.WriteLine($"Среднее арифметическое: {AMean}");
+            Console.WriteLine($"Среднее геометрическое: {GMean}");
+            Mean(A, C, out AMean, out GMean);
+            Console.WriteLine("A, C:");
+            Console.WriteLine($"Среднее арифметическое: {AMean}");
+            Console.WriteLine($"Среднее геометрическое: {GMean}");
+            Mean(A, D, out AMean, out GMean);
+            Console.WriteLine("A, D:");
+            Console.WriteLine($"Среднее арифметическое: {AMean}");
+            Console.WriteLine($"Среднее геометрическое: {GMean}");
+
+        }
+        static void Mean(double X, double Y, out double AMean, out double GMean)
+        {
+            AMean = (X + Y) / 2;
+            GMean = Math.Sqrt(X * Y);
+        }
+    }
+}
